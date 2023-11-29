935776d3fd31b3b269ad58041a96809d0facd240
Subhashis <113136062+Subhashis360@users.noreply.github.com>
2023-11-30 04:08:43 +0530

    Add files via upload

---
+++ multihook.py
@@ -0,0 +1,95 @@
+import subprocess
+import frida
+import time
+import sys
+
+def on_message(message, payload):
+    if message['type'] == 'send':
+        print(f"Encountered: {message['payload']}")
+
+
+def list_apps(device_id):
+    try:
+        adb_command = f"adb -s {device_id} shell pm list packages -f"
+        result = subprocess.run(adb_command.split(), capture_output=True, text=True)
+        app_list = result.stdout.strip().split('\n')
+        packages = [line.split(':')[-1].split('=')[-1] for line in app_list]
+        print("Installed Packages:")
+        for idx, package in enumerate(packages):
+            print(f"{idx}: {package}")
+        return packages
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+def select_package(apps):
+    try:
+        selection = int(input("Enter the number corresponding to the package to hook: "))
+        if selection < 0 or selection >= len(apps):
+            print("Invalid selection.")
+            return None
+        return apps[selection]
+    except ValueError:
+        print("Invalid input. Enter a number.")
+        return None
+
+
+
+def start_frida_script(app_package, js_file1, js_file2):
+    try:
+        device = frida.get_usb_device(1)
+
+        pid = device.spawn([app_package])
+        device.resume(pid)
+        time.sleep(1)
+        process = device.attach(pid)
+
+        with open(js_file1, 'r') as file:
+            script_code = file.read()
+        script1 = process.create_script(script_code)
+
+        with open(js_file2, 'r') as file:
+            script_code = file.read()
+        script2 = process.create_script(script_code)
+
+        script1.on('message', on_message)
+        script2.on('message', on_message)
+        script1.load()
+        script2.load()
+
+        sys.stdin.read()
+            
+            
+    except frida.ServerNotRunningError:
+        print("Frida server not running on the device. Please start Frida server.")
+    except frida.TransportError:
+        print("Error connecting to the device. Ensure Frida server is running and accessible.")
+    except KeyboardInterrupt:
+        print("Script stopped manually.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def main():
+    adb_output = subprocess.run(['adb', 'devices'], capture_output=True, text=True)
+    devices_list = adb_output.stdout.split('\n')
+    if len(devices_list) > 1:
+        first_device = devices_list[1].split('\t')[0]
+    else:
+        print("No devices Found at ADB.")
+        return
+    
+    apps = list_apps(first_device)
+    if not apps:
+        return "No device found"
+    app_package_name = select_package(apps)
+    print("Hooking started with pakage => " + app_package_name)
+
+    sslbypass = "sslbypass.js"
+    rootbypass = "antiroot.js"
+
+    start_frida_script(app_package_name, sslbypass, rootbypass)
+
+
+if __name__ == "__main__":
+    main()
